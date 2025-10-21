4184371dc4d487807bc270847205c129af1e48b4
Gryphon5549 <firstteam5549@gmail.com>
2025-10-21 16:36:53 -0400

    made belt

---
+++ src/main/java/frc/robot/subsystems/Belt.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Belt extends SubsystemBase {
+    private SparkMax BeltMotor;
+    private SparkMaxConfig BeltConfig;
+
+    public Belt() {
+        BeltMotor = new SparkMax(Constants.BELT_MOTOR_ID, MotorType.kBrushless);
+
+        // Configure the motor
+        BeltConfig = new SparkMaxConfig();
+        BeltConfig.idleMode(IdleMode.kCoast); // Let it spin freely when not powered
+        // BeltMotor.applyConfig(BeltConfig);
+    }
+
+    /** Runs the belt motor at 50% power (forward) */
+    public void runBelt() {
+        BeltMotor.set(0.5);  // Change 0.5 to any value from -1.0 to 1.0 as needed
+    }
+
+    /** Stops the belt motor */
+    public void stopBelt() {
+        BeltMotor.set(0.0);
+    }
+}

55cf9f01c9815d6ed8f68c711771e9c79863cd92
Gryphon Robotics <firstteam5549@gmail.com>
2025-10-21 16:34:16 -0400

    add intake shoot

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,35 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
-}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase{
+    SparkMax IntakeMotor;
+    SparkMaxConfig IntakeConfig;
+
+    public Intake(){
+        IntakeMotor = new SparkMax(Constants.INTAKE_MOTOR, MotorType.kBrushless);
+        IntakeConfig = new SparkMaxConfig();
+        IntakeConfig.idleMode(IdleMode.kBrake);
+        IntakeMotor.configure(IntakeConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+
+    public void intake(){
+        IntakeMotor.set(Constants.INTAKE_SPEED);
+        // System.out.println("Intake doesn't work! :(");
+        System.out.println(Constants.INTAKE_SPEED);
+    }
+    public void off(){
+        IntakeMotor.set(0);
+        System.out.println("Intake Off");
+    }
+    
+};
